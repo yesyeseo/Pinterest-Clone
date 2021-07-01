212b6eb8dca7c1e3989bbeca9f6f61f56d9b6a8a
newave986 <kingofthefall538@gmail.com>
2021-07-01 14:48:30 +0900

    add basic templates
    settings.py TEMPLATES 'DIRS'

--- pargmatic/accountapp/views.py
+++ pargmatic/accountapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello world')
+    return render(request, 'base.html')
