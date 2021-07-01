7ddef7d1553afa19ef4162fcdedaae07b39514df
newave986 <kingofthefall538@gmail.com>
2021-07-01 14:34:54 +0900

    env - hide secret key

--- pargmatic/pargmatic/settings.py
+++ pargmatic/pargmatic/settings.py
@@ -12,15 +12,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os, environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m9ba4e14mpa&-s&k!&@n=ws$$h^pyab&@$mi2-*8j@y7=9u5#_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
