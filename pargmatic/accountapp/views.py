6798783bf1fd517d4bd88eea68bc1383cf59aa9f
newave986 <kingofthefall538@gmail.com>
2021-07-05 01:56:08 +0900

    objects.all() to see all object in that db
    HttpResponseRedirect

--- pargmatic/accountapp/views.py
+++ pargmatic/accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from accountapp.models import HelloWorld
 
 # Create your views here.
@@ -14,9 +15,12 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
+        hello_world_list = HelloWorld.objects.all()
+
         # 실재로 DB에 hello world 객체를 저장하겠다는 뜻        
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
-        # 객체를 내보내 줌
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # accountapp에 있는 hello world로 재접속해라 <- urls.py 
 
     else: # GET
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
