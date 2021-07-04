9905e9a9a0631802abbd87e76bce013a52d9925d
newave986 <kingofthefall538@gmail.com>
2021-07-05 01:42:36 +0900

    POST -> save data to database
    new_hello_world = HelloWorld() in models.py

--- pargmatic/accountapp/views.py
+++ pargmatic/accountapp/views.py
@@ -1,11 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from accountapp.models import HelloWorld
 
 # Create your views here.
 
 def hello_world(request):
 
-    if request.method == "POST": 
-        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'})
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input') 
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        # 실재로 DB에 hello world 객체를 저장하겠다는 뜻        
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        # 객체를 내보내 줌
+
     else: # GET
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
