7ef0882c0c82564f1570a4d36a2b50fb0d2f464f
newave986 <kingofthefall538@gmail.com>
2021-07-06 14:03:43 +0900

    join use CreateView and form_class

--- pargmatic/accountapp/views.py
+++ pargmatic/accountapp/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
-from accountapp.models import HelloWorld
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, UpdateView
+
+from accountapp.models import HelloWorld
 
 def hello_world(request):
 
@@ -24,3 +28,13 @@ def hello_world(request):
     else: # GET
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    # reverse는 class에서 사용할 수 없다. reverse_lazy를 class에서 사용함.
+    # 기능은 별로 차이가 없음
+    template_name = 'accountapp/create.html'
