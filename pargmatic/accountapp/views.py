0a7c3d69405ecbe0e8e8e78682535ad8155ab703
newave986 <kingofthefall538@gmail.com>
2021-07-07 17:57:40 +0900

    delete user with DeleteView

--- pargmatic/accountapp/views.py
+++ pargmatic/accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, UpdateView, DetailView, UpdateView
+from django.views.generic import CreateView, UpdateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -52,3 +52,8 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
