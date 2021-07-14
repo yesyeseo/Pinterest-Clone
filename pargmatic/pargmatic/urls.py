cd282012337c73718e4f896abb6236c04a19be73
newave986 <kingofthefall538@gmail.com>
2021-07-14 13:55:52 +0900

    bug : fix accounts

--- pargmatic/pargmatic/urls.py
+++ pargmatic/pargmatic/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accountapp.urls')),
+    path('accounts/', include('accountapp.urls')),
 ]
